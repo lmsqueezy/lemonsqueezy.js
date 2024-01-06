24831a5267e6f5728bd9718b88cd4e9f1ab9465c
Branko Conjic <brankoconjic@gmail.com>
2024-01-05 20:55:33 -0500

    Add Prettier

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from "tsup";
 const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig(({ watch = false }) => ({
-  clean: true,
-  dts: true,
   entry: {
     index: "./src/index.ts",
   },
+  clean: true,
+  dts: true,
   format: ["cjs", "esm"],
   minify: isProduction,
+  outDir: "dist",
   watch,
 }));
